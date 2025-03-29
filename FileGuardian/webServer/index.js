08721e7da119342cd39651fd3bc19d02392554a8
woeakshere <dangersaitama@gmail.com>
2025-03-29 23:07:06 +0530

    Update index.js

--- FileGuardian/webServer/index.js
+++ FileGuardian/webServer/index.js
@@ -15,10 +15,8 @@
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
-
-
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const port = process.env.PORT || 3001; // or any default port you prefer
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
