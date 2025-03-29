278491e8ac669646983e95d0effd97875d55cefc
woeakshere <dangersaitama@gmail.com>
2025-03-29 23:08:36 +0530

    Update index.js

--- FileGuardian/webServer/index.js
+++ FileGuardian/webServer/index.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * This code snippet is part of the FileShareBot by @nabilanavab.
@@ -15,8 +14,9 @@
 
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3001; // Define the port first
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-const port = process.env.PORT || 3001; // or any default port you prefer
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
